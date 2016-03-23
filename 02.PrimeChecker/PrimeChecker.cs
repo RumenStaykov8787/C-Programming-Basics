715bc5f65a9006c7a5b389f01439d87f515b2d78
RumenStaykov8787 <rumen_staykov@mail.bg>
2016-03-23 18:34:41 +0200

    Added files via upload

---
+++ 02.PrimeChecker/PrimeChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class PrimerCheckerMethod
+{
+    static void Main()
+    {
+        Console.Write("Number : ");
+        double number = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Is it prime : {0}", IsItPrime(number));
+
+    }
+    static bool IsItPrime(double number)
+    {
+        bool IsPrime = true;
+        int maxDivider = (int)Math.Sqrt(number);
+        if (number == 0 || number == 1)
+        {
+            IsPrime = false;
+            return IsPrime;
+        }
+        for (int i = 2; i <= maxDivider; i++)
+        {
+            if (number % i == 0)
+            {
+                IsPrime = false;
+                break;
+            }
+        }
+        return IsPrime;
+    }
+}
