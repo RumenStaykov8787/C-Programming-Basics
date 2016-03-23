49beaa64d9542712de3f5656e9264fe3a0b022bd
RumenStaykov8787 <rumen_staykov@mail.bg>
2016-03-23 18:34:07 +0200

    Added files via upload

---
+++ 01.FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class FibonacciNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Fib(n));
+    }
+    static int Fib(int number)
+    {
+        int firstMember = 1;
+        int secondMember = 0;
+        int result = 0;
+        for (int i = 0; i <= number; i++)
+        {
+            if (number == 0 || number == 1)
+            {
+                return firstMember;
+            }
+            else
+            {
+                result = firstMember + secondMember;
+                firstMember = secondMember;
+                secondMember = result;
+            }
+        }
+        return result;
+    }
+}
